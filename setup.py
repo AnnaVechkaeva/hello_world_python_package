d932435d7f9a6ed7e1c6976174a8076af8f673f7
AnnaVechkaeva <vechkaevaanna@gmail.com>
2021-02-24 11:31:59 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as reqfile:
+	requirements = reqfile.read().split('\n')
+
 setup(
     name='hello_world_python_package',
     url='https://github.com/AnnaVechkaeva/hello_world_python_package',
     author='Anna Vechkaeva',
     author_email='vechkaevaanna@gmail.com',
     packages=find_packages(),
-    install_requires=['numpy'],
+    install_requires=requirements,
     version='0.1',
     license='',
     description='My first python package',
